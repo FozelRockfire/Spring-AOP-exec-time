88351b256141c487894ec20af58547de225d0e4e
FozelRockfire <126056156+FozelRockfire@users.noreply.github.com>
2024-03-29 18:37:10 +0500

    Initial commit

---
+++ src/test/java/com/t1study/aopspring/AopspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1study.aopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
